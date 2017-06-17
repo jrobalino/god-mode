7405cfd39e224f84e81ada405ade3f1aaae4b351
jrobalino <javier.robalino@gmail.com>
2017-06-17 00:31:33 -0500

    feat: Add intro level.

---
+++ Assets/Scripts/Intro/IntroManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour {
+
+	int potsThrown = 0;
+
+	public AudioSource levelCleared;
+	public GameObject exit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void checkPots()
+	{
+		potsThrown++;
+		if (potsThrown == 3)
+		{
+			levelCleared.Play();
+			exit.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/Intro/Pot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+	public AudioSource goodJob;
+	public IntroManager introManager;
+
+	Vector3 startPosition;
+	Quaternion startRotation;
+
+	// Use this for initialization
+	void Start()
+	{
+		startPosition = transform.position;
+		startRotation = transform.rotation;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.y < -1 && transform.position.y > -1.1)
+		{
+			goodJob.Play();
+		}
+
+		if (transform.position.y < -25)
+		{
+			killPot();
+		}
+	}
+
+	void killPot()
+	{
+		gameObject.SetActive(false);
+		transform.position = startPosition;
+		transform.rotation = startRotation;
+		introManager.checkPots();
+	}
+}
+
+
